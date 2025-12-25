32a29d087dee3fa09245d7b1dc715583bf3f4b3a
Sundaram-Katare <sundaramkatare53@gmail.com>
2025-12-25 17:13:22 +0530

    Added images

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -9,21 +9,21 @@ export default function Features() {
                 <div className="hidden md:block  relative flex items-center gap-6 mt-10">
                     <Card
                         title="Smart Suggestions"
-                        img="https://png.pngtree.com/png-vector/20220613/ourmid/pngtree-in-the-grasp-of-descending-starsa-flat-design-approach-vector-png-image_31517590.png"
+                        img="/magic.png"
                         classname=" md:-left-40 lg:-left-90 absolute z-[20] md:-bottom-10 lg:-bottom-20"
                         rotate={-28}
                         initalRotate={28}
                     />
                     <Card
                         title="Bug Detection"
-                        img="https://icons.veryicon.com/png/o/miscellaneous/first-icon/bug-33.png"
+                        img="/bug.png"
                         classname=" bg-gradient-to-b from-[#FF7070] to-red-950 "
                         rotate={0}
                         initalRotate={0}
                     />
                     <Card
                         title="Style Alignment"
-                        img="https://www.pngplay.com/wp-content/uploads/5/Thug-Life-Glasses-Transparent-Background.png"
+                        img="/glasses.png"
                         classname="md:-right-40 lg:-right-90 absolute z-[20] md:-bottom-10 lg:-bottom-20"
                         rotate={28}
                         initalRotate={-28}
@@ -33,21 +33,21 @@ export default function Features() {
                 <div className=" md:hidden flex flex-col space-y-2">
                   <Card
                         title="Smart Suggestions"
-                        img="https://png.pngtree.com/png-vector/20220613/ourmid/pngtree-in-the-grasp-of-descending-starsa-flat-design-approach-vector-png-image_31517590.png"
+                        img="/magic.png"
                         classname=" md:-left-40 lg:-left-90  md:-bottom-10 lg:-bottom-20"
                         rotate={-8}
                         initalRotate={8}
                     />
                     <Card
                         title="Bug Detection"
-                        img="https://icons.veryicon.com/png/o/miscellaneous/first-icon/bug-33.png"
+                        img="/bug.png"
                         classname=" bg-gradient-to-b from-[#FF7070] to-red-950 "
                         rotate={0}
                         initalRotate={0}
                     />
                     <Card
                         title="Style Alignment"
-                        img="https://www.pngplay.com/wp-content/uploads/5/Thug-Life-Glasses-Transparent-Background.png"
+                        img="/glasses.png"
                         classname="md:-right-40 lg:-right-90  md:-bottom-10 lg:-bottom-20"
                         rotate={8}
                         initalRotate={-8}
